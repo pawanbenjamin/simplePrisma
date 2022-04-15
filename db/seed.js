64783bcc86122ba70987b51bbcf73dc83c448931
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-04-14 21:48:09 -0400

    drop and create tables

--- db/seed.js
+++ db/seed.js
@@ -1,6 +1,47 @@
-const { puppies, owners, tricks, puppies_tricks } = require('./seedData')
+const prisma = require('./prisma')
 
 const { Puppies } = require('./models')
+
+const { puppies } = require('./seedData')
+
+const dropTables = async () => {
+  console.log(`Dropping tables...`)
+  await prisma.$executeRaw`DROP TABLE IF EXISTS puppies_tricks;`
+  await prisma.$executeRaw`DROP TABLE IF EXISTS tricks;`
+  await prisma.$executeRaw`DROP TABLE IF EXISTS puppies;`
+  await prisma.$executeRaw`DROP TABLE IF EXISTS owners;`
+}
+
+const createTables = async () => {
+  console.log(`Creating tables...`)
+  await prisma.$executeRaw`
+  CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR (255) NOT NULL
+  );`
+  await prisma.$executeRaw`
+  CREATE TABLE puppies (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR (255) NOT NULL,
+    email VARCHAR (255) UNIQUE NOT NULL,
+    "isCute" BOOLEAN DEFAULT true,
+    age INTEGER,
+    "ownerId" INTEGER REFERENCES owners(id)
+  );`
+  await prisma.$executeRaw`
+  CREATE TABLE tricks (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR
+  );`
+  await prisma.$executeRaw`
+  CREATE TABLE puppies_tricks (
+    id SERIAL PRIMARY KEY,
+    puppy_id INTEGER REFERENCES puppies(id),
+    trick_id INTEGER REFERENCES tricks(id),
+    UNIQUE(puppy_id, trick_id)
+  );
+`
+}
 
 const seedDb = async () => {
   console.log('creating pups...')
@@ -8,6 +49,16 @@ const seedDb = async () => {
   console.log('Pups:', pups)
 }
 
-seedDb()
+const initDb = async () => {
+  try {
+    await dropTables()
+    await createTables()
+    await seedDb()
+  } catch (error) {
+    console.error(error)
+  } finally {
+    await prisma.$disconnect()
+  }
+}
 
-console.log()
+initDb()
